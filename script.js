e0f09345197e11acb81b98ec93cac8a9c355b6ec
thorbenschreyer <thorben.schreyer@web.de>
2026-03-18 14:57:03 +0100

    add open close function for header menu

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+let menuIsOpen = false;
+
 function init() {
     loadHtmlPage("all-content-area", "standard_layout.html")
+    loadHtmlPage("content", "./footerpages/legal_notice.html")
 }
 
 
@@ -13,3 +16,34 @@ async function loadHtmlPage(divID, pagefile) {
     html = await response.text();
     document.getElementById(divID).innerHTML = html
 }
+
+/**
+* This function opens and closes the menu by replacing classes 
+ */
+function openCloseHeaderMenu() {
+    let headerClass = document.getElementById("header-menu")
+ if (!menuIsOpen) {
+    headerClass.classList.replace("header-menu-hide", "header-menu")
+    menuIsOpen = true 
+ } else {
+    headerClass.classList.replace("header-menu", "header-menu-hide")
+    menuIsOpen = false
+ }
+}
+
+/**
+* Allows you to close the menu by clicking outside of it
+ */
+document.addEventListener("click", function(event) {
+    let menu = document.getElementById("header-menu");
+    let button = document.querySelector(".help-and-logout button");
+
+    if (
+        menuIsOpen &&
+        !menu.contains(event.target) &&
+        !button.contains(event.target)
+    ) {
+        menu.classList.replace("header-menu", "header-menu-hide");
+        menuIsOpen = false;
+    }
+});
