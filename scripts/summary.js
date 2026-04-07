efc1b22dd990bc836436c7a98db0127b1709b9d2
thorbenschreyer <thorben.schreyer@web.de>
2026-04-07 22:14:05 +0200

    refector scripts

--- scripts/summary.js
+++ scripts/summary.js
@@ -14,8 +14,8 @@ function findNumberOfTask(taskdefinition) {
 /**
  * Rendering the numbers for the summary board
  */
-  async function setTaskSummaryInformation() {
-  await loadTasks(); 
+async function setTaskSummaryInformation() {
+  await loadTasks();
   let tasksInBoard = tasks.length;
   let tasksInProgress = findNumberOfTask("in-progress");
   let awaitingFeedback = findNumberOfTask("await-feedback");
@@ -29,7 +29,6 @@ function findNumberOfTask(taskdefinition) {
   document.getElementById("awaiting-feedback").innerText = awaitingFeedback;
   document.getElementById("task-todo").innerText = todo;
   document.getElementById("task-done").innerText = done;
-
   document.getElementById("todo-date").innerText = numberofSameDate;
   document.getElementById("date-in-Summary").innerText = formattedDate;
 
@@ -37,7 +36,7 @@ function findNumberOfTask(taskdefinition) {
 }
 
 /**
- * Sorts all tasks by date. 
+ * Sorts all tasks by date.
  * If none exist, the call is intercepted.
  * @returns -
  */
@@ -51,18 +50,25 @@ function sortTodoforDate() {
     return;
   }
 
-  toDoSOrted = toDoArray.sort((a, b) => new Date(a.dueDate) - new Date(b.dueDate),);
+  toDoSOrted = toDoArray.sort(
+    (a, b) => new Date(a.dueDate) - new Date(b.dueDate),
+  );
   nexttoDoPriority = toDoArray[0].priority;
 
-  numberofSameDate = toDoArray.filter((sameDayToDo) => sameDayToDo.dueDate === toDoArray[0].dueDate,).length;
+  numberofSameDate = toDoArray.filter(
+    (sameDayToDo) => sameDayToDo.dueDate === toDoArray[0].dueDate,
+  ).length;
   nextToDoDate = toDoArray[0].dueDate;
 
   const date = new Date(nextToDoDate);
-  formattedDate = date.toLocaleDateString("de-DE", { day: "2-digit", month: "long" }) + ", " + date.getFullYear();
+  formattedDate =
+    date.toLocaleDateString("de-DE", { day: "2-digit", month: "long" }) +
+    ", " +
+    date.getFullYear();
 }
 
 /**
- * Sets the priority flag for the next task 
+ * Sets the priority flag for the next task
  */
 function setOpticalPriority() {
   let prio = document.getElementById("prio-img");
